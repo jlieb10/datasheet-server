96de053e28ddca10197bc468a76adfdb92573bda
Joshua Lieberman <joshuathomaslieberman@gmail.com>
2018-12-03 14:19:41 +0000

    [MISC]

--- src/initialize.js
+++ src/initialize.js
@@ -6,8 +6,6 @@ import config from './config'
 const { googleSheets } = config
 const { sheets, privateKey, email } = googleSheets
 
-console.log(process.env);
-
 function authenticate (_fetcher) {
   return _fetcher.fetcher.authenticate(process.env.SERVICE_ACCOUNT_EMAIL, process.env.SERVICE_ACCOUNT_PRIVATE_KEY).then(msg => {
     console.log(msg)
