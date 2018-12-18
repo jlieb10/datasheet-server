7dc5dd179ff0c978e8da0afb18d9228f9035326b
Joshua Lieberman <joshuathomaslieberman@gmail.com>
2018-12-18 15:54:06 +0000

    [WIP] encryption feature flag

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -1,11 +1,20 @@
-#!/bin/bash
-echo "Encrypting .env file for Travis..."
+# saturates env variables
+source .env 
+export ENCRYPT
 
-# confirm that user has access to forensic architecture datasheet
-# if [ ! -z "$(travis repos | grep "forensic-architecture/datasheet-server")" ]
-# 	echo "No travis encrypt possible, skipping this step."
-# 	exit 0
-# fi
+# confirm that user wants to use encryption feature
+if [ "$ENCRYPT" = false ]; then
+	echo "============================================================================================"
+	echo "ENCRYPT flag set to false, skipping this step."
+	echo "============================================================================================"
+	return 0
+fi
+
+echo ""
+echo "If you do not have access to push to the forensic-architecture repository,"
+echo "please update your git remote configuration"
+echo ""
+echo "Encrypting .env file for Travis..."
 
 # confirm travis is installed
 if [ ! hash travis 2>/dev/null ]; then
@@ -15,7 +24,7 @@ if [ ! hash travis 2>/dev/null ]; then
 	echo "After installing, make sure that you login with:"
 	echo "\ttravis login --pro"
 	echo "============================================================================================"
-	exit 3
+	return 0
 fi
 
 # confirm there is a .env file to encrypt
@@ -23,16 +32,18 @@ if [ ! -f .env ]; then
 	echo "============================================================================================"
 	echo "ERROR: You must create a .env file and add your credentials. See .env.example for an example"
 	echo "============================================================================================"
-	exit 3
+	return 0
 fi
 
 # regex to match and delete 'before_install' and everything after it
 # necessary to delete these lines to get Travis to build for multiple accounts
-echo "creating new .travis.yml configuration"
+echo ""
+echo "Creating new .travis.yml configuration"
+
 sed -i.old '/^before_install.*/,$ d' .travis.yml
-echo "old config file saved as .travis.yml.old"
 
 travis encrypt-file .env --add --force --org
 git add .env.enc
 git add .travis.yml
+echo ""
 echo ".env.enc created and added to commit"
